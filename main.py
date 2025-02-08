2f71b09559acd62acf0d0491f92b69b6cbc6b4d3
AdryanMiiler <miojochan876@gmail.com>
2025-02-08 00:10:21 -0300

    Criacao Inicial
    Criacao do login python fase inicial.
    criacao do html e css, junto com a estilizacao.

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask('__name__')
+app.config['SECRET_KEY'] = 'IGNORE'
+
+@app.route('/')
+
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods =['POST'])
+def login():
+
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    
+    if nome == 'adryan' and senha == '123':
+        return render_template('home.html')
+    else:
+        return redirect('/')
+
+    return redirect('/')
+
+
+
+
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
